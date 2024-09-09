7ba9745aa26f882e72c8df9b2fdaea2ba32b351a
JuanpiRiv <juanpirivero8@gmail.com>
2024-09-09 19:52:07 -0300

    page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// PASO 1 -AGREGAR GIT PAGES CON REACT EN JSON : INSTALAR EL NPM INSTALL GH-PAGES --SAVE-DEV
+// PASO 2 -JSON   ENTRE "dev" , " build" + "predeploy":"npm run build",
+//    "deploy": "gh-pages -d dist",
+// PASO 3 -AJUSTAMOS VITE ======>
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ReactJSeccomerce",
   plugins: [react()],
 })
