3d79342f142a25de8d86e6a3aee5f56e9fd9e55c
JuanpiRiv <juanpirivero8@gmail.com>
2024-09-25 21:03:33 -0300

    C:/Program Files/Git/

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ReactJSeccomerce',
+  base: '/ReactJSeccomerce/',
   plugins: [react()],
   server: {
     historyApiFallback: true,
